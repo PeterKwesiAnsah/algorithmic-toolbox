8cc90e4cd43b770b3b5482959ddf1ab8e194fe85
Ansah1234 <ansahpeter123@yahoo.com>
2021-04-07 12:52:49 +0100

    using helpers

--- max_pairwise_product.js
+++ max_pairwise_product.js
@@ -2,30 +2,33 @@
 
 const readline = require('readline');
 const rl = readline.createInterface({
-    input: process.stdin,
-    terminal: false
+	input: process.stdin,
+	terminal: false,
 });
 
 process.stdin.setEncoding('utf8');
-rl.once('line', () => {
-    rl.on('line', readLine);
+rl.once('line', (line) => {
+	rl.on('line', readLine);
 });
 
-function readLine (line) {
-    const arr = line.toString().split(' ').map(Number);
+function readLine(line) {
+	const arr = line.toString().split(' ').map(Number);
 
-    console.log(max(arr));
-    process.exit();
+	console.log(max(arr));
+	process.exit();
 }
 
 function max(arr) {
-    // write your code here
-    const max_one = Math.max(...arr);
-    const index = arr.findIndex((num) => Number(num) === Number(max_one));
-    arr.splice(index, 1);
-    const max_two = Math.max(...arr);
-    return max_one * max_two;
-    
+	// write your code here
+	const max_one = Math.max(...arr);
+	const index = arr.findIndex((num) => Number(num) === Number(max_one));
+	arr.splice(index, 1);
+	let max_two = Math.max(...arr);
+	if (max_two === -Infinity) {
+		max_two = 1;
+	}
+
+	return max_one * max_two;
 }
 
 module.exports = max;
